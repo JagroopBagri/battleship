00eb66f01f4d4bfdfc48e9366c00eb722490960f
Jagroop <jagroopbagri5@gmail.com>
2022-02-11 13:05:04 -0800

    Working on DOM elements and overall gameplay functionality

--- src/dom.js
+++ src/dom.js
@@ -22,15 +22,68 @@ export const initializeDOMElements = () => {
     buttonSection: document.querySelector('.game-setup'),
   };
 };
-export const boardArrayToDOM = (boardArray, playerBoard) => {
+// function that appends the gameboard to the DOM
+export const boardArrayToDOM = (boardArray, playerBoard, computer = false) => {
+  // make an array that holds all existing squares
+  if (computer === false) {
+    let squares = Array.from(document.querySelectorAll('.square'));
+    // iterate thru squares array and delete all squares
+    if (squares.length != 0) {
+      squares.forEach((square) => square.remove());
+    }
+  }
+  if (computer === true) {
+    let squares = Array.from(document.querySelectorAll('.computerSquare'));
+    // iterate thru squares array and delete all squares
+    if (squares.length != 0) {
+      squares.forEach((square) => square.remove());
+    }
+  }
+  // go thru board array
   boardArray.forEach((array, y) => {
+    // go thru each array in board array
     array.forEach((element, x) => {
+      // create a square div for each element in array
       let square = document.createElement('div');
+      // create a div to hold the text
       let squareText = document.createElement('div');
-      square.classList.add('square');
+      // give player ones squares a class
+      if (computer === false) {
+        square.classList.add('square');
+      }
+      // if this board is made for computer player then add a different class
+      if (computer === true) {
+        square.classList.add('computerSquare');
+      }
+      // if the square represents a hit or missed spot then display it
+      if (element === 'hit' || element === 'miss') {
+        squareText.textContent = element;
+      }
+      // if the square represents an empty unattacked spot then show nothing
+      if (element === '-') {
+        squareText.textContent = '';
+      }
+      //if the square represents player ones ship then display it
+      if (
+        computer === false &&
+        element != 'hit' &&
+        element != 'miss' &&
+        element != '-'
+      ) {
+        squareText.textContent = '•';
+      }
+      //if the square represents a computers ship then dont display it
+      if (
+        computer === true &&
+        element != 'hit' &&
+        element != 'miss' &&
+        element != '-'
+      ) {
+        squareText.textContent = '';
+      }
+      // add id to square that shows the square coordinates
       square.id = x + ' ' + y;
       playerBoard.appendChild(square);
-      squareText.textContent = element;
       square.appendChild(squareText);
     });
   });
--- src/index.js
+++ src/index.js
@@ -10,7 +10,21 @@ const startApp = () => {
   let playerGameBoard = createGameBoard();
   let computerGameBoard = createGameBoard();
   boardArrayToDOM(playerGameBoard.boardArray, dom.playerBoard);
-  boardArrayToDOM(computerGameBoard.boardArray, dom.computerBoard);
+  boardArrayToDOM(computerGameBoard.boardArray, dom.computerBoard, true);
+  // place player 1 ships
+  playerGameBoard.placeShip(5, 'x', 0, 0);
+  playerGameBoard.placeShip(4, 'y', 5, 1);
+  playerGameBoard.placeShip(3, 'x', 7, 6);
+  playerGameBoard.placeShip(2, 'y', 9, 4);
+  // append player 1 ships to the DOM
+  boardArrayToDOM(playerGameBoard.boardArray, dom.playerBoard);
+  // place computer ships
+  computerGameBoard.placeShip(5, 'x', 0, 0);
+  computerGameBoard.placeShip(4, 'y', 5, 1);
+  computerGameBoard.placeShip(3, 'x', 7, 6);
+  computerGameBoard.placeShip(2, 'y', 9, 4);
+  // append computer ships to the DOM
+  boardArrayToDOM(computerGameBoard.boardArray, dom.computerBoard, true);
 };
 
 startApp();
