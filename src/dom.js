2043dabe97272867077c867bce0704568c8764f3
Jagroop <jagroopbagri5@gmail.com>
2022-02-13 15:41:58 -0800

    Completed basic game functionality

--- src/dom.js
+++ src/dom.js
@@ -53,7 +53,7 @@ export const boardArrayToDOM = (boardArray, playerBoard, computer = false) => {
   });
 };
 // function that updates board array
-export const updateBoard = (boardArray, playerBoard, computer = false) => {
+export const updateBoard = (boardArray, computer = false) => {
   // if this is player 1 and not computer
   if (computer === false) {
     // identify all squares in dom and assort them in an array
@@ -125,61 +125,63 @@ export const updateBoard = (boardArray, playerBoard, computer = false) => {
 };
 
 // function that adds event listener to gameboard squares
-export const addEventListenerToBoard = (DOMArray, gameboard) => {
-  array.forEach((square) => {
-    square.addEventListener('click', () => {});
+export const addEventListenerToGameBoard = (
+  player1Squares,
+  computerSquares,
+  playerGameBoard,
+  computerGameBoard,
+  turn,
+  gameResult
+) => {
+  // iterate thru computers array of squares
+  computerSquares.forEach((square) => {
+    // add event listener to each element in array
+    square.addEventListener('click', () => {
+      // if it's not the player 1's turn turn and the game is not over then do nothing
+      if (turn !== 1 || gameResult !== 1) {
+        return;
+      } else {
+        // retrieve the y coordinate of the square
+        let y = square.dataset.yaxis;
+        // retrieve the x coordinate of the square
+        let x = square.dataset.xaxis;
+        // computer gameboard recieves an attack
+        let result = computerGameBoard.receiveAttack(x, y);
+        // if the result is an error then return
+        if (result === 'Error') {
+          return;
+        } else if (result === 'Game Over') {
+          gameResult = 0;
+          updateBoard(computerGameBoard.boardArray, true);
+          turn = 0;
+          return;
+        } else {
+          // switch to computers turn
+          turn = 2;
+          // make variable for computers attack result
+          let computerResult;
+          // update the computer board
+          updateBoard(computerGameBoard.boardArray, true);
+          // make computer choose random coordinates on opponents board
+          do {
+            let randomY = getRandomNum(0, 9);
+            let randomX = getRandomNum(0, 9);
+            computerResult = playerGameBoard.receiveAttack(randomX, randomY);
+          } while (computerResult === 'Error');
+          if (computerResult === 'Game Over') {
+            gameResult = 0;
+            updateBoard(playerGameBoard.boardArray);
+            turn = 0;
+            return;
+          } else {
+            turn = 1;
+            updateBoard(playerGameBoard.boardArray);
+          }
+        }
+      }
+    });
   });
 };
-
-/*
-boardArrayToDOM = (boardArray, playerBoard, computer = false) => {
-    // go thru board array
-    boardArray.forEach((array, y) => {
-      // go thru each array in board array
-      array.forEach((element, x) => {
-        // create a square div for each element in array
-        let square = document.createElement('div');
-        // create a div to hold the text
-        let squareText = document.createElement('div');
-        // give player ones squares a class
-        if (computer === false) {
-          square.classList.add('square');
-        }
-        // if this board is made for computer player then add a different class
-        if (computer === true) {
-          square.classList.add('computerSquare');
-        }
-        // if the square represents a hit or missed spot then display it
-        if (element === 'hit' || element === 'miss') {
-          squareText.textContent = element;
-        }
-        // if the square represents an empty unattacked spot then show nothing
-        if (element === '-') {
-          squareText.textContent = '';
-        }
-        //if the square represents player ones ship then display it
-        if (
-          computer === false &&
-          element != 'hit' &&
-          element != 'miss' &&
-          element != '-'
-        ) {
-          squareText.textContent = '•';
-        }
-        //if the square represents a computers ship then dont display it
-        if (
-          computer === true &&
-          element != 'hit' &&
-          element != 'miss' &&
-          element != '-'
-        ) {
-          squareText.textContent = '';
-        }
-        // add id to square that shows the square coordinates
-        square.id = x + ' ' + y;
-        playerBoard.appendChild(square);
-        square.appendChild(squareText);
-      });
-    }); 
-    
-    */
+const getRandomNum = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
--- src/index.js
+++ src/index.js
@@ -3,28 +3,51 @@
 import { createShip } from './ships';
 import { createGameBoard } from './gameboard';
 import { createPlayer } from './player';
-import { initializeDOMElements, boardArrayToDOM, updateBoard } from './dom';
+import {
+  initializeDOMElements,
+  boardArrayToDOM,
+  updateBoard,
+  addEventListenerToGameBoard,
+} from './dom';
 
 const startApp = () => {
+  // turn 1 means its the players turn turn 2 means its the computers turn turn 0 means the game is over
+  let turn = 1;
+  // game result
+  let gameResult = 1;
   let dom = initializeDOMElements();
   let playerGameBoard = createGameBoard();
   let computerGameBoard = createGameBoard();
   boardArrayToDOM(playerGameBoard.boardArray, dom.playerBoard);
   boardArrayToDOM(computerGameBoard.boardArray, dom.computerBoard, true);
+  // array of player 1 tiles on gameboard
+  const player1Squares = Array.from(document.querySelectorAll('.square'));
+  // array of computer tiles on gameboard
+  const computerSquares = Array.from(
+    document.querySelectorAll('.computerSquare')
+  );
   // place player 1 ships
   playerGameBoard.placeShip(5, 'x', 0, 0);
   playerGameBoard.placeShip(4, 'y', 5, 1);
   playerGameBoard.placeShip(3, 'x', 7, 6);
   playerGameBoard.placeShip(2, 'y', 9, 4);
   // update player 1 game board
-  updateBoard(playerGameBoard.boardArray, dom.playerBoard);
+  updateBoard(playerGameBoard.boardArray);
   // place computer ships
   computerGameBoard.placeShip(5, 'x', 0, 0);
   computerGameBoard.placeShip(4, 'y', 5, 1);
   computerGameBoard.placeShip(3, 'x', 7, 6);
   computerGameBoard.placeShip(2, 'y', 9, 4);
   // update computers gameboard
-  updateBoard(computerGameBoard.boardArray, dom.computerBoard, true);
+  updateBoard(computerGameBoard.boardArray, true);
+  addEventListenerToGameBoard(
+    player1Squares,
+    computerSquares,
+    playerGameBoard,
+    computerGameBoard,
+    turn,
+    gameResult
+  );
 };
 
 startApp();
